d2f77a26fe5578c6a85153dc21ec495dc7a11bd5
RachanaDevi <rachanadevi18@gmail.com>
2020-05-11 19:04:46 +0530

    Removed alert

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,11 +1,10 @@
 var about_project_dropdown= $("#dropdown_menu");
 $(document).ready( function() {
     if ( $(window).width() < 992) {
-    	alert('WINDOW WIDTH LESS THAN');
+    	
      $('#dropdown_menu').removeClass('dropdown-menu');
     }
     else {
-    	alert('WINDOW WIDTH GREATER THAN');
     	$('#dropdown_menu').addClass('dropdown-menu');
     }
  });
@@ -13,11 +12,11 @@ $(document).ready( function() {
  $(window).resize(function() {
     /*If browser resized, check width again */
     if ($(window).width() < 992) {
-    	alert('WINDOW WIDTH LESS THAN NEW');
+    	
     $('#dropdown_menu').removeClass('dropdown-menu');
     }
     else {
-    	alert('WINDOW WIDTH GREATER THAN');
+    	
     		$('#dropdown_menu').addClass('dropdown-menu');
     }
  });
