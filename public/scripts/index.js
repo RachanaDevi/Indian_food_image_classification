0b542f1de1f7329bf5844f1a4caf54093dddf289
RachanaDevi <rachanadevi18@gmail.com>
2020-05-12 19:59:30 +0530

    Added smooth scrolling when 'About Project' is clicked

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,29 +1,8 @@
-var selectBody = $('body');
-var selectNavbarCollapse = $('.navbar-collapse');
-
-var heightNavbarCollapsed = $('.navbar').outerHeight(true);
-var heightNavbarExpanded = 0;
-
-paddingSmall();
-
-selectNavbarCollapse.on('show.bs.collapse', function () {
-  if (heightNavbarExpanded == 0 ) heightNavbarExpanded = heightNavbarCollapsed + $(this).outerHeight(true);
-  paddingGreat();
-})
-selectNavbarCollapse.on('hide.bs.collapse', function () {
-  paddingSmall();
-})
-
-$(window).resize( function () {
-  if (( document.documentElement.clientWidth > 767 ) && selectNavbarCollapse.hasClass('in') ) {
-    selectNavbarCollapse.removeClass('in').attr('aria-expanded','false');
-    paddingSmall();
-  }
+$(document).ready(function() {
+    $('a[href*=\\#about-project]').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $(this.hash).offset().top
+        }, 500);
+    });
 });
-
-function paddingSmall() {
-  selectBody.css('padding-top', heightNavbarCollapsed + 'px');
-}
-function paddingGreat() {
-  selectBody.css('padding-top', heightNavbarExpanded + 'px');
-}
