c99bd3ee2d21bfb7239493528bcbf08bdc304907
RachanaDevi <rachanadevi18@gmail.com>
2020-05-13 01:52:30 +0530

    Decreased speed of scrolling

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
         e.preventDefault();
         $('html, body').animate({
             scrollTop : $(this.hash).offset().top
-        }, 500);
+        }, 1500);
     });
 });
