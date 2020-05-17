d12ab03eccf265d877cfe4bfd3c36a5679f12dcc
RachanaDevi <rachanadevi18@gmail.com>
2020-05-17 23:42:37 +0530

    Added smooth scrolling for food-categories

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
     $('a[href*=\\#about-project]').on('click', function(e){
-        e.preventDefault();
+        // e.preventDefault(); //this is to hide the id on the url
+        $('html, body').animate({
+            scrollTop : $(this.hash).offset().top
+        }, 1500);
+    });
+});
+
+
+$(document).ready(function() {
+    $('a[href*=\\#food-categories]').on('click', function(e){
+        // e.preventDefault();
         $('html, body').animate({
             scrollTop : $(this.hash).offset().top
         }, 1500);
