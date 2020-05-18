5ce5fc5d29de246ff317a47f35c6afe8963092d8
RachanaDevi <rachanadevi18@gmail.com>
2020-05-19 03:25:50 +0530

    Successfully displayed json data

--- index.js
+++ index.js
@@ -13,44 +13,28 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res){
+	req['Content-Type']='application/json';		
 
-
+	res.setHeader('Content-Type', 'application/json');		
 	form.append( 'file', fs.createReadStream('output.png'),
 	 {filename: 'sabudhana_sample.jpg', contentType: 'image/png'} );
-	form.submit('http://ec2-34-226-213-76.compute-1.amazonaws.com/home', function(err, res) {
+	form.submit('http://ec2-34-226-213-76.compute-1.amazonaws.com/home', function(err, response,body) {
     if (err) throw err;
     console.log('Done');
-    console.log(res.statusCode);
-    console.log();
-  });
-// 	request.post({url:'http://ec2-34-226-213-76.compute-1.amazonaws.com/home',json:
-// {
-//     "file":"output.png"
-// }},
-// 	 function(err,httpResponse,body){ 
-// 	 	console.log("HEYLO");
-// 	 	console.log(body);
-// 	 	// console.log(httpResponse.statusCode);
+    var data = '';
+    response.setEncoding('utf8')
+    response.on('data',function(d){
+    	data+=d
 
-// 		if(err){
-// 			// console.log("INSIDE ERROR");
-//    			console.log(err);
-//    		}
-//     	if (!err) {
-//     		console.log("INSIDE NO ERROR");
-//     		// console.log(body);
-//      		 // var info = JSON.parse(body);
-//      		 // console.log(info.text());
-//      		       // console.log("HELLO");
-//      		       // res.send("<h1>HELLO</h1>");
-//       		res.json(body);
-//     	}
-// 	});
+    });
+    response.on('end',function(d){
+    	res.send(data);
+    })
+  
+  });
   
 });
 
-
-// request('http://google.com/doodle.png').pipe(fs.createWriteStream('doodle.png'));
 app.listen(process.env.PORT||3000,process.env.IP,function(){
 	console.log("Server has started");
 });
