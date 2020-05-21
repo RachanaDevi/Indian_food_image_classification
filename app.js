811db59e0a4f3173bae2471b7b2473c98abdd105
RachanaDevi <rachanadevi18@gmail.com>
2020-05-21 20:52:37 +0530

    Added another put route if the prediction is wrong

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser'),
 	app = express();
 
 var Food_Images_Dir = './Food_Images/';
+var food_bucket_name= "food";
 const GridFsStorage = require('multer-gridfs-storage'),
       mongoose = require('mongoose'),
       crypto = require('crypto'),
@@ -255,13 +256,46 @@ app.get("/result/:filename",(req, res)=>{
 
 
 
+function checkIfCollOfBucket(bktName,collName){
+	var bktRegex = new RegExp(bktName,"g")
+	return bktRegex.test(collName);
+}
+
+function getCollNameInsideBucket(bktName,collName){
+	console.log("CHECKING FOR %s",collName);
+	if(checkIfCollOfBucket(bktName,collName)){
+		console.log("INSIDE IF");
+		var after_bucket = bktName+"\.(.*)"
+		var getCollRegex = new RegExp(after_bucket,"g")
+		return getCollRegex.exec(collName)[1];
+
+	}
+
+}
 
 app.get("/result/:filename/no",(req,res)=>{
-	res.render("result_no",{file:req.params.filename});
+	var food_colls=[];
+	conn.db.listCollections().toArray(function (err, names) {
+      if (err) {
+      throw new Error(err);
+    } else {	
+      names.map(function(collection) {
+      	var food_coll=getCollNameInsideBucket(food_bucket_name,collection.name);
+      	if(typeof food_coll !== 'undefined'){
+      		food_colls.push(food_coll);
+      	}
+
+      });
+    console.log(food_colls);
+    res.render("result_no",{file:req.params.filename,food_colls:food_colls});
+    }
+    });
+
+	
 
 });
-
 app.put("/result/:filename/add-category/:pred_food",function(req,res){
+	console.log("HITTING HIT WITH PRED_FOOD");
 
 	Image.findOneAndUpdate({ image_filename:req.params.filename },{$set:{category:req.params.pred_food}}, function (err, foodImage) {
 		if(err){
@@ -270,7 +304,9 @@ app.put("/result/:filename/add-category/:pred_food",function(req,res){
 		else{
 			
             // console.log(foodImage);
-            foodCategoryColl = conn.model("food."+req.params.pred_food,foodCategoryImgSchema);
+            console.log("TO CHECK IF THIS IS SOMETHING OR NOT");
+            console.log(foodImage.prediction['Probabilities']);
+            foodCategoryColl = conn.model(food_bucket_name+"."+req.params.pred_food,foodCategoryImgSchema);
             newfoodInfo ={ 
             	image_id:foodImage.image_id,
     			image_filename:foodImage.image_filename,	
@@ -285,14 +321,16 @@ app.put("/result/:filename/add-category/:pred_food",function(req,res){
             		// console.log(food);
             		console.log("successfully added to database");
             		
-            		mkdirp(Food_Images_Dir+req.params.pred_food).then(function(made_dir)
-            		{
-            			  if(typeof make_dir === undefined){
-            			  	console.log("Directory is already present");
-
-            			  }
+            		mkdirp.sync(Food_Images_Dir+req.params.pred_food);
+            		// {
+            		// 	  if(typeof make_dir === undefined){
+            		// 	  	console.log("Directory is already present");
+            		// 	  }
+            		//  }); //mkdirp ending	  
+            		console.log("THIS IS THE PATH");
+            		    console.log(Food_Images_Dir+req.params.pred_food+"/"+req.params.filename);
 	 
-	            		  gfs.openDownloadStreamByName(req.params.filename).pipe(fs.createWriteStream(Food_Images_Dir+req.params.filename)).
+	            		  gfs.openDownloadStreamByName(req.params.filename).pipe(fs.createWriteStream(Food_Images_Dir+req.params.pred_food+"/"+req.params.filename)).
 	            		  on('error',function(error){
 	                         console.log(error);
 
@@ -300,7 +338,7 @@ app.put("/result/:filename/add-category/:pred_food",function(req,res){
 	                    	console.log("FINISHED");
 
 	                    });
-                    }); //mkdirp ending
+                   
             
              } //end of else
             });
@@ -311,6 +349,67 @@ app.put("/result/:filename/add-category/:pred_food",function(req,res){
 
 
 });
+
+app.put("/result/:filename/add-category/",function(req,res){
+	console.log("HERE IS REQ BODY");
+	console.log(req.body);
+	if(req.body.food_options=='other_option'){
+		req.body.food_options = req.body.other_option_text
+
+	}
+	console.log("REQ.BODY.FOOD_OPTIONS IS");
+	console.log(req.body.food_options);
+
+	Image.findOneAndUpdate({ image_filename:req.params.filename },{$set:{user_category:req.body.food_options}}, function (err, foodImage) {
+		if(err){
+			console.log(err);
+		}
+		else{
+			
+           
+            foodCategoryColl = conn.model(food_bucket_name+"."+req.body.food_options,foodCategoryImgSchema);
+            newfoodInfo ={ 
+            	image_id:foodImage.image_id,
+    			image_filename:foodImage.image_filename,	
+    			// accuracy:foodImage.prediction['Probabilities'][req.params.pred_food],
+
+            };
+            foodCategoryColl.create(newfoodInfo,function(err,food){
+            	if(err){
+            		console.log(err);
+            	}
+            	else{
+            		// console.log(food);
+            		console.log("successfully added to database");
+            		
+            		mkdirp(Food_Images_Dir+req.body.food_options);
+            		// {
+            		// 	  if(typeof make_dir === undefined){
+            		// 	  	console.log("Directory is already present");
+
+            		// 	  }
+	 
+	            		  gfs.openDownloadStreamByName(req.params.filename).pipe(fs.createWriteStream(Food_Images_Dir+req.body.food_options+"/"+req.params.filename)).
+	            		  on('error',function(error){
+	                         console.log(error);
+
+	                    }).on('finish',function(){
+	                    	console.log("FINISHED");
+
+	                    });
+                    // }); //mkdirp ending
+            
+             } //end of else
+            });
+			res.redirect("/");
+		}
+
+    });
+ // res.send("YOLLOOO");
+
+
+});
+
 app.get("/image/:filename", (req, res) => {
   const file = gfs
     .find({
