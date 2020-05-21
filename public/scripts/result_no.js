78a3ce9c1607ec738d5d8a9e6d81833e6a89834b
RachanaDevi <rachanadevi18@gmail.com>
2020-05-21 20:51:20 +0530

    Changed input class to input id

--- public/scripts/result_no.js
+++ public/scripts/result_no.js
@@ -1,12 +1,14 @@
 $(function() {
-    $('input[name="radios"]').on('click', function() {
+    $('input[id="other_option"]').on('click', function() {
     	console.log("THIS WAS TRIGGERED");
     	console.log($(this));
         if ($(this).val() == 'other_option') {
-            $('#other_option').show();
+            $('#other_option_text').show();
         }
         else {
-            $('#other_option').hide();
+            $('#other_option_text').hide();
         }
     });
 });
+
+
