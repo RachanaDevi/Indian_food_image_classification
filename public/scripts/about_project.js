17b81282b7ea7a73dd8ca044ad2a556551b584c3
RachanaDevi <rachanadevi18@gmail.com>
2020-05-23 02:10:58 +0530

    Changed threshold to 1

--- public/scripts/about_project.js
+++ public/scripts/about_project.js
@@ -1,7 +1,7 @@
 const faders = document.querySelectorAll(".fade-in"),
       sliders = document.querySelectorAll(".slide-in");
 const appearOptions = {
-  threshold:0,
+  threshold:1,
   rootMargin:"0px 0px -100px 0px"
 
 };
