1409e773065e7cf400329ec03c6715fdb802c4ae
RachanaDevi <rachanadevi18@gmail.com>
2020-05-24 16:33:00 +0530

    Removed unecessary console.log

--- public/scripts/result_no.js
+++ public/scripts/result_no.js
@@ -2,7 +2,6 @@ $("#other_option_text").val("");
 
 $(function() {
     $('input[id="other_option"]').on('click', function() {
-    	console.log("THIS WAS TRIGGERED");
     	console.log($(this));
         if ($(this).val() == 'other_option') {
             $('#other_option_text').show();
