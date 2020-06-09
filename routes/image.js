0dd464ef5c7b9a5fa7b14085fd3ef1f4af773696
RachanaDevi <rachanadevi18@gmail.com>
2020-06-10 03:54:25 +0530

    Added route to get image using filename

---
+++ routes/image.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router({mergeParams:true});
+const exported_db = require('../database.js');
+
+
+router.get("/", (req, res) => {
+  const file = gfs
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist"
+        });
+      }
+      gfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
+});
+
+module.exports = router;
