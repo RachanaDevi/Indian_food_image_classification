bac35ff90bad78e2152f07d2736da38bfcd10aa7
RachanaDevi <rachanadevi18@gmail.com>
2020-06-14 13:07:37 +0530

    Added functions in route result/:filename/add-category/:pred_food

--- routes/result.js
+++ routes/result.js
@@ -51,55 +51,123 @@ router.get("/no",(req,res)=>{
 
 });
 
+function setKeyValueForFoodCategory(category_type,food_type){
+    return {[category_type]:food_type};
+}
 
-router.put("/add-category/:pred_food",function(req,res){
-	Image.findOneAndUpdate({ image_filename:req.params.filename },{$set:{category:req.params.pred_food.toLowerCase()}}, function (err, foodImage) {
-		if(err){
-			console.log(err);
-		}
-		else{
-			
-          
-            var collectionName=(food_bucket_name+"."+req.params.pred_food.toLowerCase());
-            foodCategoryColl = conn.model(food_bucket_name+"."+req.params.pred_food.toLowerCase(),foodCategoryImgSchema,collectionName);
+function thisHasToBeNamedBetter(category_type,food_type,filename){
+    console.log("I AM HEEEREEEEEEEEEEEE");
+    food_key = food_type;
+    food_type = food_type.toLowerCase();
+
+    Image.findOneAndUpdate({ image_filename:filename },setKeyValueForFoodCategory(category_type,food_type), function (err, foodImage) {
+        if(err){
+            console.log(err);
+        }
+        else{
+            
+            // console.log(`THIS IS BEFORE USING LOWERCASE ${req.params.pred_food}`);
+            var collectionName=(food_bucket_name+"."+food_type);
+            // console.log(`THIS IS BEFORE AFTER LOWERCASE ${req.params.pred_food}`);
+
+            foodCategoryColl = conn.model(food_bucket_name+"."+food_type,foodCategoryImgSchema,collectionName);
             newfoodInfo = { 
-            	image_id:foodImage.image_id,
-    			    image_filename:foodImage.image_filename,	
-          		accuracy:foodImage.prediction['Probabilities'][req.params.pred_food],
+                image_id:foodImage.image_id,
+                    image_filename:foodImage.image_filename,    
+                accuracy:foodImage.prediction['Probabilities'][food_key],
 
             };
 
 
             foodCategoryColl.create(newfoodInfo,function(err,food){
-            	if(err){
-            		console.log(err);
-            	}
+                if(err){
+                    console.log(err);
+                }
 
-            	else{
-            		
-            		console.log("successfully added to database");
-            		if (!fs.existsSync(Food_Images_Dir+req.params.pred_food.toLowerCase())){
-                    fs.mkdirSync(Food_Images_Dir+req.params.pred_food.toLowerCase());
+                else{
+                    
+                    console.log("successfully added to database");
+                    if (!fs.existsSync(Food_Images_Dir+food_type)){
+                    fs.mkdirSync(Food_Images_Dir+food_type);
                 }
 
 
-	 
-	            	gfs.openDownloadStreamByName(req.params.filename).pipe(fs.createWriteStream(Food_Images_Dir+req.params.pred_food.toLowerCase()+"/"+req.params.filename)).
-	            		  on('error',function(error){
-	                         console.log(error);
+     
+                    gfs.openDownloadStreamByName(filename).pipe(fs.createWriteStream(Food_Images_Dir+food_type+"/"+filename)).
+                          on('error',function(error){
+                             console.log(error);
 
-	                    }).on('finish',function(){
-	                    	console.log("FINISHED");
+                        }).on('finish',function(){
+                            console.log("FINISHED");
 
-	                    });
+                        });
                    
             
              } //end of else
-            });
-			res.redirect("/");
-		}
+            }); //end of ImageFindOne
+            
+        }
 
     });
+
+
+}
+router.put("/add-category/:pred_food",function(req,res){
+    console.log("HEY THIS IS BEFORE PROMISE WAS MADE");
+    // req.params.pred_food = req.params.pred_food.toLowerCase();
+    var promise = new Promise((resolve,reject)=>{
+        resolve(thisHasToBeNamedBetter("category",req.params.pred_food,req.params.filename));
+});
+    promise.then(res.redirect("/"));
+	// Image.findOneAndUpdate({ image_filename:req.params.filename },setKeyValueForFoodCategory("category",req.params.pred_food), function (err, foodImage) {
+	// 	if(err){
+	// 		console.log(err);
+	// 	}
+	// 	else{
+			
+ //            console.log(`THIS IS BEFORE USING LOWERCASE ${req.params.pred_food}`);
+ //            var collectionName=(food_bucket_name+"."+req.params.pred_food);
+ //            console.log(`THIS IS BEFORE AFTER LOWERCASE ${req.params.pred_food}`);
+
+ //            foodCategoryColl = conn.model(food_bucket_name+"."+req.params.pred_food,foodCategoryImgSchema,collectionName);
+ //            newfoodInfo = { 
+ //            	image_id:foodImage.image_id,
+ //    			    image_filename:foodImage.image_filename,	
+ //          		accuracy:foodImage.prediction['Probabilities'][req.params.pred_food],
+
+ //            };
+
+
+ //            foodCategoryColl.create(newfoodInfo,function(err,food){
+ //            	if(err){
+ //            		console.log(err);
+ //            	}
+
+ //            	else{
+            		
+ //            		console.log("successfully added to database");
+ //            		if (!fs.existsSync(Food_Images_Dir+req.params.pred_food)){
+ //                    fs.mkdirSync(Food_Images_Dir+req.params.pred_food.);
+ //                }
+
+
+	 
+	//             	gfs.openDownloadStreamByName(req.params.filename).pipe(fs.createWriteStream(Food_Images_Dir+req.params.pred_food+"/"+req.params.filename)).
+	//             		  on('error',function(error){
+	//                          console.log(error);
+
+	//                     }).on('finish',function(){
+	//                     	console.log("FINISHED");
+
+	//                     });
+                   
+            
+ //             } //end of else
+ //            });
+	// 		res.redirect("/");
+	// 	}
+
+ //    });
 
 
 });
